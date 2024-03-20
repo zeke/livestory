10752894e69725e8f9c3a0e7e455c11a1762b760
cbh123 <charlieholtz@gmail.com>
2024-03-19 17:12:00 -0700

    vibe is much better

--- index.js
+++ index.js
@@ -1,60 +1,59 @@
-import { spawn } from 'child_process'
-import stripAnsi from 'strip-ansi'
+import { spawn } from "child_process";
+import stripAnsi from "strip-ansi";
 // import Replicate from "replicate";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 // import download from 'download'
-import path from 'path'
-import WebSocket from 'ws'
-import fs from 'fs'
-dotenv.config()
+import path from "path";
+import WebSocket from "ws";
+import fs from "fs";
+dotenv.config();
 
-const ws = new WebSocket('ws://100.76.41.44:5000')
+const ws = new WebSocket("ws://100.76.41.44:5000");
 // const replicate = new Replicate();
-const outputDirectory = './output'
-let frameNumber = 0
+const outputDirectory = "./output";
+let frameNumber = 0;
 
 // clean up output dir
 fs.readdir(outputDirectory, (err, files) => {
-  if (err) throw err
+  if (err) throw err;
 
   for (const file of files) {
     fs.unlink(path.join(outputDirectory, file), (err) => {
-      if (err) throw err
-    })
+      if (err) throw err;
+    });
   }
-})
+});
 
-ws.on('open', () => {
-  console.log('Connected to WebSocket server')
-})
+ws.on("open", () => {
+  console.log("Connected to WebSocket server");
+});
 
-ws.on('message', (data) => {
-  console.log(data, 'log')
-  const uniqueFileName = `coreweave_output_${frameNumber}.jpg`
-  fs.writeFileSync(path.join(outputDirectory, uniqueFileName), data)
-  fs.writeFileSync(path.join(outputDirectory, '_latest.jpg'), data)
-  console.log(`Image saved as ${uniqueFileName}`)
-})
+ws.on("message", (data) => {
+  const uniqueFileName = `coreweave_output_${frameNumber}.jpg`;
+  fs.writeFileSync(path.join(outputDirectory, uniqueFileName), data);
+  fs.writeFileSync(path.join(outputDirectory, "_latest.jpg"), data);
+  console.log(`Image saved as ${uniqueFileName}`);
+  frameNumber += 1;
+});
 
-async function makeImage (prompt) {
+async function makeImage(prompt) {
   const lastImagePath = path.join(
     outputDirectory,
-    `coreweave_output_${frameNumber}.jpg`
-  )
+    `coreweave_output_${frameNumber - 1}.jpg`
+  );
 
-  let image
+  let image;
 
   if (frameNumber === 0) {
     // image = fs.readFileSync("./circle.jpg").toString("base64");
-    image = undefined
+    image = undefined;
   } else if (!fs.existsSync(lastImagePath)) {
-    image = undefined
+    console.log("CANT FIND IMAGE!!!!!!!!!!", lastImagePath);
   } else {
-    image = fs.readFileSync(lastImagePath).toString('base64')
+    image = fs.readFileSync(lastImagePath).toString("base64");
   }
 
-  ws.send(JSON.stringify({ prompt, image }))
-  frameNumber += 1
+  ws.send(JSON.stringify({ prompt, image }));
 }
 
 // async function makeReplicateImage(prompt) {
@@ -75,31 +74,30 @@ async function makeImage (prompt) {
 //   }
 // }
 
-async function generateImage (speechData) {
+async function generateImage(speechData) {
   const crazyWhisperPrompt = stripAnsi(speechData)
-    .replace(/[\r\n]+/g, ' ')
-    .replace(/\[.*?\]/g, '')
-    .split('.')
+    .replace(/[\r\n]+/g, " ")
+    .replace(/\[.*?\]/g, "")
+    .split(".")
     .map((sentence) => sentence.trim())
     .filter((sentence, index, self) => self.indexOf(sentence) === index)
-    .join('. ')
-    .trim()
+    .join(". ")
+    .trim();
 
   if (!crazyWhisperPrompt.length) {
-    console.log('no prompt yet!')
-    return
+    console.log("no prompt yet!");
+  } else {
+    await makeImage(crazyWhisperPrompt);
   }
 
-  await makeImage(crazyWhisperPrompt)
   //   await makeReplicateImage(crazyWhisperPrompt);
 }
 
 // Whisper.cpp stuff
-const listen = spawn('sh', ['./listen.sh'])
+const listen = spawn("sh", ["./listen.sh"]);
 
-listen.stdout.on('data', async (chunk) => {
-  console.log(String(chunk))
-  generateImage(String(chunk))
-})
+listen.stdout.on("data", async (chunk) => {
+  generateImage(String(chunk));
+});
 
-listen.stdout.on('end', () => {})
+listen.stdout.on("end", () => {});
